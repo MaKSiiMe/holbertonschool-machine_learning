9c23e1c25aeecce1f3eb52f4d63054de9a0788e7
MaKSiiMe <maxime.truel@gmail.com>
2025-08-04 14:36:04 +0200

    Formatage du code pour améliorer la lisibilité de la fonction add_matrices2D

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -6,4 +6,5 @@ def add_matrices2D(mat1, mat2):
     """Adds two 2D matrices element-wise."""
     if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
         return None
-    return [[x + y for x, y in zip(row1, row2)] for row1, row2 in zip(mat1, mat2)]
+    return [[x + y for x, y in zip(row1, row2)]
+            for row1, row2 in zip(mat1, mat2)]
