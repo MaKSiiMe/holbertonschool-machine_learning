167ad0bdc805cec5c5dc09d08f3d9504154c1753
MaKSiiMe <maxime.truel@gmail.com>
2025-08-04 13:52:15 +0200

    Ajout de la fonction matrix_shape pour calculer la forme d'une matrice

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+def matrix_shape(matrix):
+    """Calculates the shape of a matrix."""
+    if not isinstance(matrix, list) or not matrix:
+        return []
+
+    shape = []
+    while isinstance(matrix, list):
+        shape.append(len(matrix))
+        if not matrix:
+            break
+        matrix = matrix[0]
+    
+    return shape
