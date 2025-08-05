43093604b7c8b2256d56db979d9093cd6c930357
MaKSiiMe <maxime.truel@gmail.com>
2025-08-05 09:55:39 +0200

    Suppression de la vérification du type pour les matrices dans les fonctions np_shape et np_transpose

--- math/linear_algebra/11-the_western_exchange.py
+++ math/linear_algebra/11-the_western_exchange.py
@@ -1,12 +1,8 @@
 #!/usr/bin/env python3
 """Module to return the transpose of a numpy array."""
-import numpy as np
 
 
 def np_transpose(matrix):
     """Returns the transpose of a numpy array."""
-
-    if not isinstance(matrix, np.ndarray):
-        raise TypeError("matrix must be a numpy.ndarray")
 
     return matrix.T
