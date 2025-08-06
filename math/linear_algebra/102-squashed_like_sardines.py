5f634d147f4ae9e23a7f059a80de0d7f296e5251
MaKSiiMe <maxime.truel@gmail.com>
2025-08-06 10:31:47 +0200

    Ajout de la fonction cat_matrices pour concaténer deux matrices selon un axe spécifié

---
+++ math/linear_algebra/102-squashed_like_sardines.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+
+def cat_matrices(mat1, mat2, axis=0):
+    """Concatenate two matrices along a specified axis."""
+
+    if not isinstance(mat1, list) or not isinstance(mat2, list):
+        return None
+
+    if axis == 0:
+        return mat1 + mat2
+
+    if len(mat1) == 0 or len(mat2) == 0:
+        return mat1 if len(mat2) == 0 else mat2
+
+    if len(mat1[0]) != len(mat2[0]):
+        return None
+
+    return [row1 + row2 for row1, row2 in zip(mat1, mat2)]
