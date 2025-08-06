d581398f7b22f55ada4c4e9b4cdf33082e4b946f
MaKSiiMe <maxime.truel@gmail.com>
2025-08-06 10:09:46 +0200

    Ajout d'un module pour découper un tableau numpy selon les axes fournis

---
+++ math/linear_algebra/100-slice_like_a_ninja.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Module to slice a numpy array like a ninja."""
+
+
+def np_slice(matrix, axes={}):
+    """Slices a numpy array according to the axes provided."""
+
+    slices = tuple(slice(*axes.get(i, (None, None, None))) for i in range(matrix.ndim))
+
+    return matrix[slices]
