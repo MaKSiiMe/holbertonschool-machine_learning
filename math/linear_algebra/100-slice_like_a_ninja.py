02574a32559b0d7bc3f59c83eabffc973e0d03a9
MaKSiiMe <maxime.truel@gmail.com>
2025-08-06 10:14:50 +0200

    Formatage du code pour améliorer la lisibilité dans la fonction np_slice

--- math/linear_algebra/100-slice_like_a_ninja.py
+++ math/linear_algebra/100-slice_like_a_ninja.py
@@ -5,6 +5,7 @@
 def np_slice(matrix, axes={}):
     """Slices a numpy array according to the axes provided."""
 
-    slices = tuple(slice(*axes.get(i, (None, None, None))) for i in range(matrix.ndim))
+    slices = tuple(slice(*axes.get(i, (None, None, None)))
+                   for i in range(matrix.ndim))
 
     return matrix[slices]
