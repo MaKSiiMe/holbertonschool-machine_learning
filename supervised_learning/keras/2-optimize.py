2d9dfed51fb76083759a4c29181076e5246f7cfc
MaKSiiMe <maxime.truel@gmail.com>
2025-09-04 16:20:22 +0200

    Mise à jour de la docstring de la fonction optimize_model pour améliorer la lisibilité en répartissant le texte sur plusieurs lignes.

--- supervised_learning/keras/2-optimize.py
+++ supervised_learning/keras/2-optimize.py
@@ -4,7 +4,8 @@ import tensorflow.keras as K
 
 
 def optimize_model(network, alpha, beta1, beta2):
-    """Sets up Adam optimization for a Keras model with categorical crossentropy loss and accuracy metrics."""
+    """Sets up Adam optimization for a Keras model
+    with categorical crossentropy loss and accuracy metrics."""
     adam = K.optimizers.Adam(learning_rate=alpha, beta_1=beta1, beta_2=beta2)
     network.compile(optimizer=adam,
                     loss='categorical_crossentropy',
