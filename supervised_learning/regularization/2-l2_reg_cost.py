48484eade908802480932d01d6e7702a8876b8b9
MaKSiiMe <maxime.truel@gmail.com>
2025-09-19 10:48:36 +0200

    Correction du calcul des coûts par couche dans la fonction l2_reg_cost pour inclure les pertes L2.

--- supervised_learning/regularization/2-l2_reg_cost.py
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -7,6 +7,6 @@ def l2_reg_cost(cost, model):
     """Calculates the cost of a neural network with L2 regularization"""
     l2_losses = model.losses
 
-    costs_per_layer = [cost] + l2_losses
+    costs_per_layer = [cost + l2_loss for l2_loss in l2_losses]
 
     return tf.stack(costs_per_layer)
