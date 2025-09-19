44f0dbee19a1f8d38fd4902f04be77a61945d993
MaKSiiMe <maxime.truel@gmail.com>
2025-09-19 10:45:36 +0200

    Amélioration de la docstring de la fonction l2_reg_create_layer pour une meilleure lisibilité

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -4,13 +4,15 @@ import tensorflow as tf
 
 
 def l2_reg_create_layer(prev, n, activation, lambtha):
-    """Creates a neural network layer in TensorFlow that includes L2 regularization"""
+    """Creates a neural network layer in TensorFlow
+    that includes L2 regularization"""
     l2_regularizer = tf.keras.regularizers.l2(lambtha)
 
     layer = tf.keras.layers.Dense(
         units=n,
         activation=activation,
-        kernel_initializer=tf.keras.initializers.VarianceScaling(scale=2.0, mode='fan_avg'),
+        kernel_initializer=tf.keras.initializers.VarianceScaling(
+            scale=2.0, mode='fan_avg'),
         kernel_regularizer=l2_regularizer
     )
 
