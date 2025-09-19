fd4f6cf4bc5b9f3a521e03ad945827cfa2f4e847
MaKSiiMe <maxime.truel@gmail.com>
2025-09-19 10:43:35 +0200

    Ajout des fichiers pour le coût de régularisation L2 et la création d'une couche avec régularisation L2.

---
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""2. L2 Regularization Cost"""
+import tensorflow as tf
+
+
+def l2_reg_cost(cost, model):
+    """Calculates the cost of a neural network with L2 regularization"""
+    l2_losses = model.losses
+
+    costs_per_layer = [cost] + l2_losses
+
+    return tf.stack(costs_per_layer)
