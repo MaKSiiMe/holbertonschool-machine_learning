ea6180d92f31dcde0008cddd9b767470558f0b14
MaKSiiMe <maxime.truel@gmail.com>
2025-09-25 15:39:43 +0200

    Correction du calcul des valeurs de padding dans les fonctions de convolution pour gérer correctement les cas de padding 'same'.

--- math/convolutions_and_pooling/5-convolve.py
+++ math/convolutions_and_pooling/5-convolve.py
@@ -10,8 +10,8 @@ def convolve(images, kernels, padding='same', stride=(1, 1)):
     sh, sw = stride
 
     if padding == 'same':
-        ph = ((h - 1) * sh + kh - h) // 2
-        pw = ((w - 1) * sw + kw - w) // 2
+        ph = max((h - 1) * sh + kh - h, 0) // 2
+        pw = max((w - 1) * sw + kw - w, 0) // 2
     elif padding == 'valid':
         ph = pw = 0
     else:
