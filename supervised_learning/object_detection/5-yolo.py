8ad20ad72f110905ea43a8a0390702901f781b5c
MaKSiiMe <maxime.truel@gmail.com>
2025-10-23 16:06:02 +0200

    Refactor la classe Yolo pour améliorer la lisibilité et l'efficacité du traitement des images, en mettant à jour la méthode preprocess_images et en simplifiant le traitement des sorties.

--- supervised_learning/object_detection/5-yolo.py
+++ supervised_learning/object_detection/5-yolo.py
@@ -1,85 +1,70 @@
 #!/usr/bin/env python3
-"""5. Preprocess images"""
-import tensorflow.keras as K
+"""Yolo v3 algorithm for object detection."""
+
 import numpy as np
+from tensorflow import keras as K
 import os
 import cv2
 
 
 class Yolo:
-    """Class that uses the Yolo v3 algorithm to perform object detection"""
-
+    """Class defining yolo v3 algorithm for object detection."""
     def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
-        """
-        Constructor for Yolo class
-        """
+        """Class constructor for Yolo v3 algorithm."""
         self.model = K.models.load_model(model_path)
-        with open(classes_path, 'r') as f:
-            self.class_names = [line.strip() for line in f]
+
+        f = open(classes_path, 'r')
+        self.class_names = f.read().split("\n")[:-1]
+
         self.class_t = class_t
         self.nms_t = nms_t
         self.anchors = anchors
 
     def process_outputs(self, outputs, image_size):
-        """
-        Process Darknet outputs
-        """
+        """ Processes the outputs.
+        Returns the boundary boxes, box confidence's
+        and class probabilities for each output."""
+        image_height, image_width = image_size
         boxes = []
         box_confidences = []
         box_class_probs = []
 
-        image_height, image_width = image_size
-
         for i, output in enumerate(outputs):
             grid_height, grid_width, anchor_boxes, _ = output.shape
+            box_xy, box_wh = output[..., :2], output[..., 2:4]
+            box_confidence, class_prob = output[..., 4:5], output[..., 5:]
 
-            box_confidence = 1 / (1 + np.exp(-output[..., 4:5]))
-            box_confidences.append(box_confidence)
+            box_xy = 1 / (1 + np.exp(-box_xy))
+            box_confidence = 1 / (1 + np.exp(-box_confidence))
+            class_prob = 1 / (1 + np.exp(-class_prob))
 
-            box_class_prob = 1 / (1 + np.exp(-output[..., 5:]))
-            box_class_probs.append(box_class_prob)
+            grid_x, grid_y = np.meshgrid(
+                np.arange(grid_width), np.arange(grid_height)
+            )
+            grid = np.stack(
+                (grid_x, grid_y), axis=-1
+            ).reshape((grid_height, grid_width, 1, 2))
 
-            box_xy = output[..., :2]
-            box_wh = output[..., 2:4]
+            box_xy = (box_xy + grid) / [grid_width, grid_height]
+            anchors = self.anchors[i].reshape((1, 1, anchor_boxes, 2))
+            box_wh = np.exp(box_wh) * anchors
+            box_x1y1 = box_xy - (box_wh / 2)
+            box_x2y2 = box_xy + (box_wh / 2)
+            box = np.concatenate([box_x1y1, box_x2y2], axis=-1)
 
-            col = np.arange(grid_width).reshape(1, grid_width, 1)
-            col = np.tile(col, [grid_height, 1, anchor_boxes])
-            row = np.arange(grid_height).reshape(grid_height, 1, 1)
-            row = np.tile(row, [1, grid_width, anchor_boxes])
-
-            sig_x = 1 / (1 + np.exp(-box_xy[..., 0]))
-            box_xy[..., 0] = (sig_x + col) / grid_width
-            sig_y = 1 / (1 + np.exp(-box_xy[..., 1]))
-            box_xy[..., 1] = (sig_y + row) / grid_height
-
-            anchor_wh = self.anchors[i]
-            anchor_wh = anchor_wh.reshape(1, 1, anchor_boxes, 2)
-            box_wh = anchor_wh * np.exp(box_wh)
-
-            input_width = self.model.input.shape[1]
-            input_height = self.model.input.shape[2]
-            box_wh[..., 0] /= input_width
-            box_wh[..., 1] /= input_height
-
-            box_x1 = (box_xy[..., 0] - box_wh[..., 0] / 2) * image_width
-            box_y1 = (box_xy[..., 1] - box_wh[..., 1] / 2) * image_height
-            box_x2 = (box_xy[..., 0] + box_wh[..., 0] / 2) * image_width
-            box_y2 = (box_xy[..., 1] + box_wh[..., 1] / 2) * image_height
-
-            box = np.zeros(output[..., :4].shape)
-            box[..., 0] = box_x1
-            box[..., 1] = box_y1
-            box[..., 2] = box_x2
-            box[..., 3] = box_y2
+            box[..., 0] *= image_width
+            box[..., 1] *= image_height
+            box[..., 2] *= image_width
+            box[..., 3] *= image_height
 
             boxes.append(box)
+            box_confidences.append(box_confidence)
+            box_class_probs.append(class_prob)
 
-        return (boxes, box_confidences, box_class_probs)
+        return boxes, box_confidences, box_class_probs
 
     def filter_boxes(self, boxes, box_confidences, box_class_probs):
-        """
-        Filter boxes based on class threshold
-        """
+        """Filters bounding boxes by confidence score, class probabilities"""
         filtered_boxes = []
         box_classes = []
         box_scores = []
@@ -89,6 +74,7 @@ class Yolo:
             box_class = np.argmax(scores, axis=-1)
             box_score = np.max(scores, axis=-1)
             mask = box_score >= self.class_t
+
             filtered_boxes.append(boxes[i][mask])
             box_classes.append(box_class[mask])
             box_scores.append(box_score[mask])
@@ -97,105 +83,94 @@ class Yolo:
         box_classes = np.concatenate(box_classes, axis=0)
         box_scores = np.concatenate(box_scores, axis=0)
 
-        return (filtered_boxes, box_classes, box_scores)
+        return filtered_boxes, box_classes, box_scores
 
     def non_max_suppression(self, filtered_boxes, box_classes, box_scores):
-        """
-        Apply Non-Maximum Suppression
-        """
+        """Selects bounding boxes based on non-maximum suppression."""
         box_predictions = []
         predicted_box_classes = []
         predicted_box_scores = []
 
-        unique_classes = np.unique(box_classes)
+        unique_labels = np.unique(box_classes)
 
-        for cls in unique_classes:
-            idx = np.where(box_classes == cls)
+        for label in unique_labels:
+            mask = box_classes == label
+            class_boxes = filtered_boxes[mask]
+            class_scores = box_scores[mask]
 
-            class_boxes = filtered_boxes[idx]
-            class_scores = box_scores[idx]
-
-            sorted_idx = np.argsort(class_scores)[::-1]
-            class_boxes = class_boxes[sorted_idx]
-            class_scores = class_scores[sorted_idx]
-
-            keep_boxes = []
-            keep_scores = []
+            indices = np.argsort(class_scores)[::-1]
+            class_boxes = class_boxes[indices]
+            class_scores = class_scores[indices]
 
             while len(class_boxes) > 0:
-                keep_boxes.append(class_boxes[0])
-                keep_scores.append(class_scores[0])
+                highest_conf_box = class_boxes[0]
+                highest_conf_score = class_scores[0]
 
-                if len(class_boxes) == 1:
-                    break
+                box_predictions.append(highest_conf_box)
+                predicted_box_classes.append(label)
+                predicted_box_scores.append(highest_conf_score)
 
-                x1 = np.maximum(class_boxes[0, 0], class_boxes[1:, 0])
-                y1 = np.maximum(class_boxes[0, 1], class_boxes[1:, 1])
-                x2 = np.minimum(class_boxes[0, 2], class_boxes[1:, 2])
-                y2 = np.minimum(class_boxes[0, 3], class_boxes[1:, 3])
+                x1 = np.maximum(highest_conf_box[0], class_boxes[1:, 0])
+                y1 = np.maximum(highest_conf_box[1], class_boxes[1:, 1])
+                x2 = np.minimum(highest_conf_box[2], class_boxes[1:, 2])
+                y2 = np.minimum(highest_conf_box[3], class_boxes[1:, 3])
 
-                intersection = np.maximum(0, x2 - x1) * np.maximum(0, y2 - y1)
+                intersection_x = np.maximum(0, x2 - x1)
+                intersection_y = np.maximum(0, y2 - y1)
+                intersection_area = intersection_x * intersection_y
 
-                box1_area = ((class_boxes[0, 2] - class_boxes[0, 0]) *
-                             (class_boxes[0, 3] - class_boxes[0, 1]))
-                box2_area = ((class_boxes[1:, 2] - class_boxes[1:, 0]) *
-                             (class_boxes[1:, 3] - class_boxes[1:, 1]))
-                union = box1_area + box2_area - intersection
+                box_w = (class_boxes[:, 2] - class_boxes[:, 0])
+                box_h = (class_boxes[:, 3] - class_boxes[:, 1])
+                box_area = box_w * box_h
 
-                iou = intersection / union
+                highest_conf_w = (highest_conf_box[2] - highest_conf_box[0])
+                highest_conf_h = (highest_conf_box[3] - highest_conf_box[1])
+                highest_conf_area = highest_conf_w * highest_conf_h
+                union = box_area[1:] + highest_conf_area - intersection_area
 
-                keep_mask = iou < self.nms_t
-                class_boxes = class_boxes[1:][keep_mask]
-                class_scores = class_scores[1:][keep_mask]
+                iou = intersection_area / union
 
-            if len(keep_boxes) > 0:
-                box_predictions.append(np.array(keep_boxes))
-                predicted_box_classes.append(np.full(len(keep_boxes), cls))
-                predicted_box_scores.append(np.array(keep_scores))
-
-        box_predictions = np.concatenate(box_predictions, axis=0)
-        predicted_box_classes = np.concatenate(predicted_box_classes, axis=0)
-        predicted_box_scores = np.concatenate(predicted_box_scores, axis=0)
-
-        return (box_predictions, predicted_box_classes, predicted_box_scores)
+                iou_mask = iou < self.nms_t
+                class_boxes = class_boxes[1:][iou_mask]
+                class_scores = class_scores[1:][iou_mask]
+        return (
+            np.array(box_predictions),
+            np.array(predicted_box_classes),
+            np.array(predicted_box_scores)
+        )
 
     @staticmethod
     def load_images(folder_path):
-        """
-        Load images from a folder
-        """
+        """Loads images from folder.
+        Returns list of images and list of images paths."""
         images = []
-        image_paths = []
-
-        for filename in sorted(os.listdir(folder_path)):
-            filepath = os.path.join(folder_path, filename)
-            if os.path.isfile(filepath):
-                image = cv2.imread(filepath)
-                if image is not None:
-                    images.append(image)
-                    image_paths.append(filepath)
-
-        return images, image_paths
+        images_paths = []
+        for filename in os.listdir(folder_path):
+            image_path = os.path.join(folder_path, filename)
+            image = cv2.imread(image_path)
+            if image is not None:
+                images.append(image)
+                images_paths.append(image_path)
+        return images, images_paths
 
     def preprocess_images(self, images):
-        """
-        Preprocess images for the Darknet model
-        """
-        input_h = self.model.input.shape[1]
-        input_w = self.model.input.shape[2]
-
-        pimages = []
+        """Resizes images with inter-cubic interpolation
+        and normalizes pixel values."""
         image_shapes = []
+        pimages = []
+
+        input_w, input_h = self.model.input.shape[1:3]
 
         for image in images:
-            image_shapes.append(np.array([image.shape[0], image.shape[1]]))
-
-            # Resize image with inter-cubic interpolation
-            # cv2.resize expects (width, height)
-            pimage = cv2.resize(image, (input_w, input_h),
-                                interpolation=cv2.INTER_CUBIC)
-            pimage = pimage / 255.0
-            pimages.append(pimage)
+            image_height, image_width = image.shape[:2]
+            image_shapes.append((image_height, image_width))
+            resized_images = cv2.resize(
+                image,
+                (input_w, input_h),
+                interpolation=cv2.INTER_CUBIC
+            )
+            norm_image = resized_images / 255.0
+            pimages.append(norm_image)
 
         pimages = np.array(pimages)
         image_shapes = np.array(image_shapes)
