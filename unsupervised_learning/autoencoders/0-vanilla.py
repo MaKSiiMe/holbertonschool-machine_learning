a00c4efa54e2cf46e8473949929076c662a13230
MaKSiiMe <maxime.truel@gmail.com>
2025-12-18 14:07:18 +0100

    Ajout de l'implémentation d'un autoencodeur simple et d'un autoencodeur sparse avec Keras, incluant la régularisation L1 pour le second.

---
+++ unsupervised_learning/autoencoders/0-vanilla.py
@@ -0,0 +1,38 @@
+import tensorflow.keras as keras
+
+def autoencoder(input_dims, hidden_layers, latent_dims):
+    """
+    Crée un autoencoder simple (vanilla) avec Keras.
+
+    Args:
+        input_dims (int): dimension de l'entrée
+        hidden_layers (list): nb de noeuds pour chaque couche cachée de l'encodeur
+        latent_dims (int): dimension de l'espace latent
+
+    Returns:
+        encoder, decoder, auto
+    """
+    # Encoder
+    input_encoder = keras.Input(shape=(input_dims,))
+    x = input_encoder
+    for nodes in hidden_layers:
+        x = keras.layers.Dense(nodes, activation='relu')(x)
+    latent = keras.layers.Dense(latent_dims, activation='relu')(x)
+    encoder = keras.Model(inputs=input_encoder, outputs=latent)
+
+    # Decoder
+    input_decoder = keras.Input(shape=(latent_dims,))
+    x = input_decoder
+    for nodes in reversed(hidden_layers):
+        x = keras.layers.Dense(nodes, activation='relu')(x)
+    output_decoder = keras.layers.Dense(input_dims, activation='sigmoid')(x)
+    decoder = keras.Model(inputs=input_decoder, outputs=output_decoder)
+
+    # Autoencoder
+    input_auto = keras.Input(shape=(input_dims,))
+    encoded = encoder(input_auto)
+    decoded = decoder(encoded)
+    auto = keras.Model(inputs=input_auto, outputs=decoded)
+    auto.compile(optimizer='adam', loss='binary_crossentropy')
+
+    return encoder, decoder, auto
---
+++ unsupervised_learning/autoencoders/1-sparse.py
@@ -0,0 +1,43 @@
+import tensorflow.keras as keras
+
+def autoencoder(input_dims, hidden_layers, latent_dims, lambtha):
+    """
+    Crée un autoencodeur sparse avec régularisation L1 sur la couche latente.
+
+    Args:
+        input_dims (int): dimension de l'entrée
+        hidden_layers (list): nb de noeuds pour chaque couche cachée de l'encodeur
+        latent_dims (int): dimension de l'espace latent
+        lambtha (float): paramètre de régularisation L1
+
+    Returns:
+        encoder, decoder, auto
+    """
+    # Encoder
+    input_encoder = keras.Input(shape=(input_dims,))
+    x = input_encoder
+    for nodes in hidden_layers:
+        x = keras.layers.Dense(nodes, activation='relu')(x)
+    latent = keras.layers.Dense(
+        latent_dims,
+        activation='relu',
+        activity_regularizer=keras.regularizers.l1(lambtha)
+    )(x)
+    encoder = keras.Model(inputs=input_encoder, outputs=latent)
+
+    # Decoder
+    input_decoder = keras.Input(shape=(latent_dims,))
+    x = input_decoder
+    for nodes in reversed(hidden_layers):
+        x = keras.layers.Dense(nodes, activation='relu')(x)
+    output_decoder = keras.layers.Dense(input_dims, activation='sigmoid')(x)
+    decoder = keras.Model(inputs=input_decoder, outputs=output_decoder)
+
+    # Autoencoder
+    input_auto = keras.Input(shape=(input_dims,))
+    encoded = encoder(input_auto)
+    decoded = decoder(encoded)
+    auto = keras.Model(inputs=input_auto, outputs=decoded)
+    auto.compile(optimizer='adam', loss='binary_crossentropy')
+
+    return encoder, decoder, auto
