5bc98ad40a03b511c05da6da4fc23ab30c6ab471
MaKSiiMe <maxime.truel@gmail.com>
2026-01-05 18:37:30 +0100

    Ajout de la documentation pour la classe Simple_GAN et ses méthodes

--- unsupervised_learning/gan/0-simple_gan.py
+++ unsupervised_learning/gan/0-simple_gan.py
@@ -1,14 +1,26 @@
 #!/usr/bin/env python3
 
+"""
+Module définissant la classe Simple_GAN pour entraîner un GAN simple
+avec Keras.
+"""
+
 import tensorflow as tf
 from tensorflow import keras
 
 
 class Simple_GAN(keras.Model):
+    """
+    Classe GAN simple utilisant Keras pour l'entraînement d'un générateur
+    et d'un discriminateur.
+    """
 
     def __init__(self, generator, discriminator, latent_generator,
                  real_examples, batch_size=200, disc_iter=2,
                  learning_rate=.005):
+        """
+        Initialise le Simple_GAN avec ses composants et hyperparamètres.
+        """
         super().__init__()
         self.latent_generator = latent_generator
         self.real_examples = real_examples
@@ -49,11 +61,17 @@ class Simple_GAN(keras.Model):
         )
 
     def get_fake_sample(self, size=None, training=False):
+        """
+        Génère un batch d'échantillons synthétiques à partir du générateur.
+        """
         if not size:
             size = self.batch_size
         return self.generator(self.latent_generator(size), training=training)
 
     def get_real_sample(self, size=None):
+        """
+        Sélectionne un batch aléatoire d'échantillons réels.
+        """
         if not size:
             size = self.batch_size
         sorted_indices = tf.range(tf.shape(self.real_examples)[0])
@@ -61,6 +79,10 @@ class Simple_GAN(keras.Model):
         return tf.gather(self.real_examples, random_indices)
 
     def train_step(self, useless_argument):
+        """
+        Effectue une étape d'entraînement GAN : plusieurs updates du
+        discriminateur, puis un update du générateur.
+        """
         # Entraînement du discriminateur
         for _ in range(self.disc_iter):
             with tf.GradientTape() as tape:
