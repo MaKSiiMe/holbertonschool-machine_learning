dc4bf8b4c3bb1b4fb7ce7aa17bbf95951bb647f8
MaKSiiMe <maxime.truel@gmail.com>
2026-03-29 22:26:30 +0200

    Ajout du paramètre show_result à la fonction d'entraînement pour afficher les résultats tous les 1000 épisodes

--- reinforcement_learning/policy_gradients/train.py
+++ reinforcement_learning/policy_gradients/train.py
@@ -6,7 +6,8 @@ Module implementing training for policy gradient agents.
 import numpy as np
 
 
-def train(env, nb_episodes, alpha=0.000045, gamma=0.98):
+def train(env, nb_episodes, alpha=0.000045, gamma=0.98,
+          show_result=False):
     """
     Trains a policy gradient agent on the given environment.
 
@@ -15,6 +16,7 @@ def train(env, nb_episodes, alpha=0.000045, gamma=0.98):
     nb_episodes: number of episodes to train for
     alpha: learning rate
     gamma: discount factor
+    show_result: if True, render every 1000 episodes
 
     Returns:
     list of scores (sum of rewards per episode)
@@ -39,7 +41,8 @@ def train(env, nb_episodes, alpha=0.000045, gamma=0.98):
         done = False
         while not done:
             action, grad = policy_gradient(state, weight)
-            next_state, reward, terminated, truncated, _ = env.step(action)
+            next_state, reward, terminated, truncated, _ = env.step(
+                action)
             done = terminated or truncated
 
             trajectory.append((grad, reward))
@@ -56,5 +59,17 @@ def train(env, nb_episodes, alpha=0.000045, gamma=0.98):
         # Print episode info
         print("Episode: {} Score: {}".format(episode, score))
         scores.append(score)
+
+        # Show result every 1000 episodes
+        if show_result and (episode + 1) % 1000 == 0:
+            test_state, _ = env.reset()
+            test_done = False
+            test_score = 0
+            while not test_done:
+                action, _ = policy_gradient(test_state, weight)
+                test_state, reward, terminated, truncated, _ = \
+                    env.step(action)
+                test_done = terminated or truncated
+                test_score += reward
 
     return scores
